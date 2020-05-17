5bbbd49d8e645026bc24a468c65df8fc22033c16
Brett Cooper <greenandgold52@gmail.com>
2020-05-17 16:40:20 -0700

    first commit to git to get project started

---
+++ app/src/main/java/com/aussieinaz/familyorganizer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aussieinaz.familyorganizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
